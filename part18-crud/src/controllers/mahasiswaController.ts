77337f3ba49a7f9d2e33d06c20504ec7a8e77cd6
Pojok Code <asep.komarudin@dataon.com>
2024-12-26 17:53:53 +0700

    enc: add mhs model

--- part18-crud/src/controllers/mahasiswaController.ts
+++ part18-crud/src/controllers/mahasiswaController.ts
@@ -1,15 +1,21 @@
-import prisma from "../utils/prisma";
 import { successResponse, errorResponse } from "../views/response";
 import { mahasiswaSchema } from "../utils/validation";
 import logger from "../utils/logger";
+import {
+  createMhs,
+  deleteMhs,
+  getAllMhs,
+  getMhsById,
+  updateMhs,
+} from "../models/mahasiswaModel";
 
 export const getAllMahasiswa = async () => {
-  const mahasiswaList = await prisma.mahasiswa.findMany();
+  const mahasiswaList = await getAllMhs();
   return successResponse("List of Mahasiswa", mahasiswaList);
 };
 
 export const getMahasiswaById = async (id: number) => {
-  const mahasiswa = await prisma.mahasiswa.findUnique({ where: { id } });
+  const mahasiswa = await getMhsById(id);
   if (!mahasiswa) return errorResponse("Mahasiswa not found", 404);
   return successResponse("Mahasiswa found", mahasiswa);
 };
@@ -32,11 +38,9 @@ export const createMahasiswa = async (req: Request) => {
 
   // Jika validasi berhasil
   try {
-    const newMahasiswa = await prisma.mahasiswa.create({
-      data: value,
-    });
+    const newMahasiswa = await createMhs(value.npm, value.nama);
     logger.info(`Mahasiswa created: ${JSON.stringify(newMahasiswa)}`);
-    return successResponse("Mahasiswa created", newMahasiswa,201);
+    return successResponse("Mahasiswa created", newMahasiswa, 201);
   } catch (err: any) {
     logger.error("Error creating Mahasiswa: " + err.message);
     return errorResponse("Error creating Mahasiswa");
@@ -47,10 +51,7 @@ export const updateMahasiswa = async (id: number, req: Request) => {
   const { npm, nama } = await req.json();
 
   try {
-    const updatedMahasiswa = await prisma.mahasiswa.update({
-      where: { id },
-      data: { npm, nama },
-    });
+    const updatedMahasiswa = await updateMhs(id, npm, nama);
     return successResponse("Mahasiswa updated", updatedMahasiswa);
   } catch (err: any) {
     logger.error("Error creating Mahasiswa: " + err.message);
@@ -60,7 +61,7 @@ export const updateMahasiswa = async (id: number, req: Request) => {
 
 export const deleteMahasiswa = async (id: number) => {
   try {
-    await prisma.mahasiswa.delete({ where: { id } });
+    await deleteMhs(id);
     return successResponse("Mahasiswa deleted", null);
   } catch (err: any) {
     logger.error("Error deleting Mahasiswa: " + err.message);
--- part18-crud/src/models/mahasiswaModel.ts
+++ part18-crud/src/models/mahasiswaModel.ts
@@ -1,6 +1,39 @@
-export class Mahasiswa {
-  constructor(public id: number, public npm: string, public nama: string) {}
-}
+import { PrismaClient, type Mahasiswa } from "@prisma/client";
 
-// Simulasi database (array sebagai penyimpanan sementara)
-export const mahasiswaList: Mahasiswa[] = [];
+const prisma = new PrismaClient();
+
+export const getAllMhs = async (): Promise<Mahasiswa[]> => {
+  return await prisma.mahasiswa.findMany();
+};
+
+export const getMhsById = async (id: number): Promise<Mahasiswa | null> => {
+  return await prisma.mahasiswa.findUnique({
+    where: { id },
+  });
+};
+
+export const createMhs = async (
+  npm: string,
+  nama: string
+): Promise<Mahasiswa> => {
+  return await prisma.mahasiswa.create({
+    data: { npm, nama },
+  });
+};
+
+export const updateMhs = async (
+  id: number,
+  npm: string,
+  nama: string
+): Promise<Mahasiswa | null> => {
+  return await prisma.mahasiswa.update({
+    where: { id },
+    data: { npm, nama },
+  });
+};
+
+export const deleteMhs = async (id: number): Promise<Mahasiswa> => {
+  return await prisma.mahasiswa.delete({
+    where: { id },
+  });
+};
